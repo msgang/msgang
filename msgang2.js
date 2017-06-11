fa0f7f4760b576de3277e0391add05dbe6828127
msgang <mdrabbyuzzaman@gmail.com>
2017-06-12 01:14:37 +0600

    Add files via upload

---
+++ msgang2.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var list = document.getElementsByClassName("personidboys");
+for (var i = 1; i <= list.length; i++) {
+    list[i].innerHTML = i;
+}
+</script>
---
+++ msgang3.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var list = document.getElementsByClassName("personidoutofgang");
+for (var i = 1; i <= list.length; i++) {
+    list[i].innerHTML = i;
+}
+</script>
